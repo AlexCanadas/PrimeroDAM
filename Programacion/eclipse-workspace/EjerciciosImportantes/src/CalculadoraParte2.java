bcdccef7be4257d6787b01d6f29669733e52ca52
AlexCanadas <alejandrocanadasgarcia@gmail.com>
2023-12-19 22:05:06 +0100

    Update

--- Programacion/eclipse-workspace/EjerciciosImportantes/src/CalculadoraParte2.java
+++ Programacion/eclipse-workspace/EjerciciosImportantes/src/CalculadoraParte2.java
@@ -8,7 +8,6 @@ public class CalculadoraParte2 {
         int opcion;
         double num1, num2, mayor, menor, resultado;
         String seguir = null;
-        double test;
         
         do { 
         	System.out.println("Ingrese primer número");
