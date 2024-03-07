1631d53b8af1270e198d5fb1dd8ca7221b29461e
AlexCanadas <alejandrocanadasgarcia@gmail.com>
2024-03-07 15:52:58 +0100

    update

--- Programacion/eclipse-workspace/EjerciciosAtrasados/src/AlquilerVehiculos/Microbuses.java
+++ Programacion/eclipse-workspace/EjerciciosAtrasados/src/AlquilerVehiculos/Microbuses.java
@@ -19,7 +19,7 @@ public class Microbuses extends Vehiculos {
 		// Calcular el precio adicional por plaza y día para coches
 		double precioAdicionalPlaza = 1.5 * (_plaza + 2) * _diasAlquiler;
 		// Calcular el precio total sumando el precio base y el precio adicional por plaza y día
-		double precioFinal = (super.getPRECIOALQUILER() * _diasAlquiler) + precioAdicionalPlaza;
+		double precioFinal = (this.precioAlquiler * _diasAlquiler) + precioAdicionalPlaza;
 		System.out.println("El precio de alquiler de este microbus con matrícula " + this.getMatricula() +
 				" es de: " + precioFinal + " euros.\n");
 				
