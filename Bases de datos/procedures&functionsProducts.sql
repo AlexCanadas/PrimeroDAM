6b4b8b03b449706689129d413172ee3ec4811bb8
AlexCanadas <alejandrocanadasgarcia@gmail.com>
2024-05-08 12:54:53 +0200

    ok

---
+++ Bases de datos/procedures&functionsProducts.sql
@@ -0,0 +1,18 @@
+--Pregunta: Crea un procedimiento que reciba el id_cliente como parámetro y devuelva todos los pedidos realizados por ese cliente.
+
+--Pregunta: Crea un procedimiento que reciba el id_representante como parámetro y devuelva el total de ventas realizadas por ese representante.
+
+--Pregunta: Crea un procedimiento que reciba el id_producto como parámetro y devuelva todos los comentarios para ese producto.
+
+--Pregunta: Crea un procedimiento que reciba una fecha de inicio y una fecha de fin, y devuelva las ventas totales realizadas entre esas fechas.
+
+--Pregunta: Crea un procedimiento que reciba el id_cliente como parámetro y devuelva el total de pagos pendientes para ese cliente.
+
+
+--Pregunta: Crea una función que reciba el id_cliente como parámetro y devuelva el total de pedidos realizados por ese cliente.
+
+--Pregunta: Crea una función que reciba el id_cliente como parámetro y devuelva el nombre del representante de ventas de ese cliente.
+
+--Pregunta: Crea una función que reciba el id_producto como parámetro y devuelva el número de comentarios positivos (valoración >= 4) para ese producto.
+
+--Pregunta: Crea una función que reciba el id_producto como parámetro y devuelva el coste total de ese producto basado en el coste unitario y el stock.
