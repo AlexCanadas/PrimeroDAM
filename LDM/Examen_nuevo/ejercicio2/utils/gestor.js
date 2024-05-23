4550ef12e4f0ca038f4b0c9e28471d9f03a00154
AlexCanadas <alejandrocanadasgarcia@gmail.com>
2024-05-23 19:06:26 +0200

    ok

--- LDM/Examen_nuevo/ejercicio2/utils/gestor.js
+++ LDM/Examen_nuevo/ejercicio2/utils/gestor.js
@@ -7,9 +7,9 @@ boton.addEventListener("click", () => {
 let tweet = tweetInput.value;
 
 output.innerHTML += `
-<div class="col">
+<div class="col mb-2">
     <div class="card" style="width: 16rem;">
-        <img src="?" class="card-img-top" alt="...">
+        <img src="./images/twitter-x.webp" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">${tweet}</h5>
         </div>
