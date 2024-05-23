2787e4483d086a0123ce4d7fa4629550911723eb
AlexCanadas <alejandrocanadasgarcia@gmail.com>
2024-05-23 14:30:48 +0200

    ok

--- Programacion/eclipse-workspace/Formulario completo Lourdes Cudad/FormatoFechaNoValidoException.java
+++
@@ -1,9 +0,0 @@
-package FormularioCompleto;
-
-public class FormatoFechaNoValidoException {
-    public static void comprobarFecha(String fecha) throws IllegalArgumentException {
-        if (!fecha.matches("\\d{2}/\\d{2}/\\d{4}")) {
-            throw new IllegalArgumentException("La fecha de nacimiento debe tener el formato dd/mm/yyyy");
-        }
-    }
-}
--- Programacion/eclipse-workspace/Formulario completo Lourdes Cudad/LongitudDNINoValidaException.java
+++
@@ -1,9 +0,0 @@
-package FormularioCompleto;
-
-public class LongitudDNINoValidaException {
-    public static void comprobarLongitud(String dni) throws IllegalArgumentException {
-        if (dni.length() != 9) {
-            throw new IllegalArgumentException("La longitud del DNI debe ser igual a 9 caracteres");
-        }
-    }
-}
